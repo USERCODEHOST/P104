8c07c04edacf4359e83fb6c279c0ae12183c7c2f
USERCODEHOST <108839721+USERCODEHOST@users.noreply.github.com>
2022-11-22 14:11:42 -0600

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,21 @@
+// random value generated
+  var y = Math.floor(Math.random()*10+1);
+
+  var x=document.getElementById("guessField").value;
+
+  if (x == y)
+  { alert("CONGRATULATIONS!!! "+playername+" YOU GUESSED IT RIGHT IN"
+  + guess + "GUESS"); 
+  guess= 1;}
+    else if(x > y) /* if guessed number is greater than actual number*/
+{guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+    else
+    { guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");}
+
+
+function playAgain(){
+    y=Math.floor(Math.random()*10+1);
+}
---
+++ login.js
@@ -0,0 +1,5 @@
+// Create login Function here.
+function Login() {
+    player_name = document.getElementById("playername").value; 
+    localStorage.setItem("player_name", player_name);
+    window.location = "gamepage.html";}
